ec44b0f8dd85ca643a05b433ccca09aaf245451c
admidral <admidral@users.noreply.github.com>
2025-01-09 23:07:58 -0800

    Added a bunch of comments

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use tokio::io::{BufWriter,BufReader,AsyncWriteExt,AsyncBufReadExt};
 use tokio::net::{TcpListener,tcp::{OwnedReadHalf,OwnedWriteHalf}};
 use tokio::sync::broadcast::{Sender,Receiver};
+
+
 #[derive(Clone,Debug)]
 struct Message{
     pub port: u16,
     pub message: String
 }
 
+// Using anyhow just so I don't need to deal with error transformation but also not use unwrap.
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // Using 8888 instead of 8080 since that was what was given in example
@@ -15,10 +18,10 @@ async fn main() -> anyhow::Result<()> {
     let sender = Sender::new(1000);
     loop {
         let (socket, socket_addr) = listener.accept().await?;
-        // Use IP port and ID;
+        // Use IP port as ID;
         let port = socket_addr.port();
         let (read, write) = socket.into_split();
-        // Immediately respond with login
+        // Make bufreader and bufwriter so we don't need to deal with try_write
         let writer = BufWriter::new(write);
         let reader = BufReader::new(read);
         tokio::spawn(read_side(port,reader,sender.clone()));
@@ -42,6 +45,7 @@ async fn read_side(port: u16,listener:BufReader<OwnedReadHalf>,tx: Sender<Messag
 
 async fn write_side(port:u16,mut writer: BufWriter<OwnedWriteHalf>,mut rx:Receiver<Message>)-> anyhow::Result<()>{
     writer.write(&format!("LOGIN:{}\n",port).into_bytes()).await?;
+     // Tested and need to flush each time otherwise we don't get the response straight away
     writer.flush().await?;
     loop{
         let message = rx.recv().await?;
