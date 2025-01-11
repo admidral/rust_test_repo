453633e085446ba1fbcbbb37384756a19ca281ff
admidral <admidral@users.noreply.github.com>
2025-01-10 22:31:18 -0800

    Move to single threaded

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,45 @@
 use tokio::io::{BufWriter,BufReader,AsyncWriteExt,AsyncBufReadExt};
-use tokio::net::{TcpListener,tcp::{OwnedReadHalf,OwnedWriteHalf}};
-use tokio::sync::broadcast::{Sender,Receiver};
+use tokio::net::TcpListener;
+use tokio::time::timeout;
+use core::time::Duration;
 
-
-#[derive(Clone,Debug)]
-struct Message{
-    pub port: u16,
-    pub message: String
-}
+// Set arbitarily to 10ms before trying next thing.
+// Idea behind this is a time_slice much like how an OS will time_split
+const TIMEOUT_DURATION:Duration = Duration::from_millis(10);
 
 // Using anyhow just so I don't need to deal with error transformation but also not use unwrap.
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // Using 8888 instead of 8080 since that was what was given in example
     let listener = TcpListener::bind("127.0.0.1:8888").await?;
-    //Assuming the send pressure isn't super high. 1000 is a very large lag factor.
-    let sender = Sender::new(1000);
+    let mut readers = Vec::new();
+    let mut writers = Vec::new();
     loop {
-        let (socket, socket_addr) = listener.accept().await?;
-        // Use IP port as ID;
-        let port = socket_addr.port();
-        let (read, write) = socket.into_split();
-        // Make bufreader and bufwriter so we don't need to deal with try_write
-        let writer = BufWriter::new(write);
-        let reader = BufReader::new(read);
-        tokio::spawn(read_side(port,reader,sender.clone()));
-        tokio::spawn(write_side(port,writer,sender.subscribe()));
-    }
-}
-
-async fn read_side(port: u16,listener:BufReader<OwnedReadHalf>,tx: Sender<Message>)-> anyhow::Result<()>{
-    let mut lines = listener.lines();
-    while let Some(line) = lines.next_line().await? {
-        let message = Message{
-            port,
-            message: line
-        };
-        tx.send(message)?;
-    }
-    Ok(())
-}
-
-
-
-async fn write_side(port:u16,mut writer: BufWriter<OwnedWriteHalf>,mut rx:Receiver<Message>)-> anyhow::Result<()>{
-    writer.write(&format!("LOGIN:{}\n",port).into_bytes()).await?;
-     // Tested and need to flush each time otherwise we don't get the response straight away
-    writer.flush().await?;
-    loop{
-        let message = rx.recv().await?;
-        if message.port == port{
-            writer.write(&format!("ACK:MESSAGE\n").into_bytes()).await?;
-            writer.flush().await?;
-        }else{
-           writer.write(&format!("Message:{} {}\n",message.port,message.message).into_bytes()).await?;
-           writer.flush().await?;
+        // First check if anyone wants to join
+        if let Some(Ok((socket, socket_addr))) = timeout(TIMEOUT_DURATION,listener.accept()).await.ok(){
+            let port = socket_addr.port();
+            let (read, write) = socket.into_split();
+            // Make bufreader and bufwriter so we don't need to deal with try_write
+            let mut writer = BufWriter::new(write);
+            let reader = BufReader::new(read);
+            writer.write(&format!("LOGIN:{}\n",port).into_bytes()).await.ok();
+            writer.flush().await.ok();
+            readers.push((port,reader.lines()));
+            writers.push((port,writer));
+        }
+        // Then for each person check their threads.
+        for (reader_port,reader) in readers.iter_mut(){
+            if let Ok(Ok(Some(line))) =  timeout(TIMEOUT_DURATION,reader.next_line()).await{
+                for (writer_port,writer) in writers.iter_mut(){
+                    if writer_port == reader_port{
+                        writer.write(&format!("ACK:MESSAGE\n").into_bytes()).await.ok();
+                        writer.flush().await.ok();
+                    }else{
+                       writer.write(&format!("Message:{} {}\n",reader_port,line).into_bytes()).await.ok();
+                       writer.flush().await.ok();
+                    }
+                }
+            }
         }
     }
 }
